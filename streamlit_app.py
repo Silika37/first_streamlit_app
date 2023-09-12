a1fd71a69af2ecd021af4d412f12ada791b9063d
Silika37 <144452458+Silika37@users.noreply.github.com>
2023-09-12 16:18:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,9 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
-
+# Allow the end user to pick a fruit
+fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('Thanks for adding', fruit_choice)
 
 
 
